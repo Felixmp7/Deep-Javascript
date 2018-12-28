4232b0e1c7ca8b00084376f0ab6559f13e330c16
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2018-12-28 13:53:25 -0400

    Implementing arrow function to submit

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 import './css/Formulario.css';
 class Formulario extends Component {
+
+  submit = e => {
+    e.preventDefault(e);
+    console.log('Submiteando');
+    var nombre = document.querySelector('#nombre').value;
+    var apellido = document.querySelector('#apellido').value;
+    var edad = document.querySelector('#edad').value;
+    console.log(nombre,apellido,edad);
+  }
   render() {
     return (
       <div className="Formulario">
         <div className="Cont-Form">
-          <form className="form" action="#">
+          <form id="form" className="form" action="#" onSubmit={this.submit}>
             <label htmlFor="nombre">Nombre</label> 
             <input name="nombre" id="nombre" type="text" placeholder="Nombre"/>
             <label htmlFor="apellido">Apellido</label> 
             <input name="apellido" id="apellido" type="text" placeholder="Apellido" />
             <label htmlFor="edad">Edad</label> 
             <input name="edad" id="edad" type="number" placeholder="Edad" />
+            <button type="submit">Validar</button>
           </form>
         </div>
       </div>
     );
   }
 }
+
+
+
+
 
 export default Formulario;
 
