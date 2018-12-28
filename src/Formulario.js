560a2ab4605ebcdd00f6014c588f8826f0080d1c
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2018-12-28 14:39:33 -0400

    Trying to deploy a data from array

--- src/Formulario.js
+++ src/Formulario.js
@@ -9,31 +9,39 @@ class Formulario extends Component {
     var apellido = document.querySelector('#apellido').value;
     var edad = document.querySelector('#edad').value;
     console.log(nombre,apellido,edad);
-
     var datos = [nombre,apellido,edad];
     console.log(datos);
+    const deploy = datos =>{
+      var resultado = document.querySelector('#resultado');
+      resultado.datos.map(dato => {
+        return <li>{dato}</li>
+      })
+    }
   }
   render() {
     return (
-      <div className="Formulario">
-        <div className="Cont-Form">
-          <form id="form" className="form" action="#" onSubmit={this.submit}>
-            <label htmlFor="nombre">Nombre</label> 
-            <input name="nombre" id="nombre" type="text" placeholder="Nombre"/>
-            <label htmlFor="apellido">Apellido</label> 
-            <input name="apellido" id="apellido" type="text" placeholder="Apellido" />
-            <label htmlFor="edad">Edad</label> 
-            <input name="edad" id="edad" type="number" placeholder="Edad" />
-            <button type="submit">Validar</button>
-          </form>
+      <div className="container">
+        <div className="Formulario">
+          <div className="Cont-Form">
+            <form id="form" className="form" action="#" onSubmit={this.submit}>
+              <label htmlFor="nombre">Nombre</label> 
+              <input name="nombre" id="nombre" type="text" placeholder="Nombre"/>
+
+              <label htmlFor="apellido">Apellido</label> 
+              <input name="apellido" id="apellido" type="text" placeholder="Apellido" />
+
+              <label htmlFor="edad">Edad</label> 
+              <input name="edad" id="edad" type="number" placeholder="Edad" />
+              <button type="submit">Enviar</button>
+            </form>
+          </div>
+        </div>
+        <br/>
+        
+        <div id="resultado">
+          {this.deploy}
         </div>
       </div>
-
-      /*<div className="resultado">
-        <ul>
-          <li>{}</li>
-        </ul>
-      </div>*/
     );
   }
 }
